836868abfe69ba2b912cb01fa6ade283465849b5
samiksha <samikshal@sahaj.ai>
2023-02-19 07:24:26 +0530

    Added getSpot function

--- src/main/kotlin/exceptions/Exceptions.kt
+++ src/main/kotlin/exceptions/Exceptions.kt
@@ -1,8 +1,6 @@
 package exceptions
 
 
-
 class ParkingLotFullException : Exception("Parking Lot is full")
 class SpotIsOccupiedException : Exception("Spot is occupied")
-
 class InvalidSpotExceptions : Exception("Spot number is Invalid")
--- src/main/kotlin/models/ParkingLot.kt
+++ src/main/kotlin/models/ParkingLot.kt
@@ -1,4 +1,17 @@
 package models
 
+import constants.MAX_CAPACITY
+import exceptions.InvalidSpotExceptions
+import exceptions.SpotIsOccupiedException
 class ParkingLot {
+
+    private val spots: MutableList<ParkingSpot?> = MutableList(MAX_CAPACITY + 1) { null }
+
+    fun getSpot(spotNumber: Int): ParkingSpot? {
+        if (spotNumber > MAX_CAPACITY || spotNumber <= 0)
+            throw InvalidSpotExceptions()
+        if (spots[spotNumber] == null)
+            spots[spotNumber] = ParkingSpot(spotNumber)
+        return spots[spotNumber]
 }
+
