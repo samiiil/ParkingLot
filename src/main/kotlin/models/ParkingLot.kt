7fbb4c65b9ad959697496bfaa19f05de2ba77f35
samiksha <samikshal@sahaj.ai>
2023-02-19 07:27:58 +0530

    Add park function for vehicle

--- src/main/kotlin/models/ParkingLot.kt
+++ src/main/kotlin/models/ParkingLot.kt
@@ -3,6 +3,7 @@ package models
 import constants.MAX_CAPACITY
 import exceptions.InvalidSpotExceptions
 import exceptions.SpotIsOccupiedException
+import entities.Vehicle
 class ParkingLot {
 
     private val spots: MutableList<ParkingSpot?> = MutableList(MAX_CAPACITY + 1) { null }
@@ -14,4 +15,13 @@ class ParkingLot {
             spots[spotNumber] = ParkingSpot(spotNumber)
         return spots[spotNumber]
 }
+    fun park(vehicle: Vehicle, spotNumber: Int): ParkingSpot {
+        val spot = getSpot(spotNumber)!!
+        if (spot.isFree()) {
+            spots[spot.getspotNumber()]?.assignVehicle(vehicle)
+            return spot
+        }
+        throw SpotIsOccupiedException()
+
+    }
 
